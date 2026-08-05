c8ae8df7ee18f0c41398589b002f4d2a86d6e3da
bushtail <evannosich@icloud.com>
2026-08-05 17:51:00 -0400

    version bump

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -13,7 +13,7 @@ public record ModMetadata : IModMetadata
     public string Author { get; init; } = "Crackbone";
     public List<string>? Contributors { get; init; } = ["Crackbone", "The WTT Team"];
     public Version Version { get; init; } = new("3.0.0");
-    public Range SptVersion { get; init; } = new("~4.0.0");
+    public Range SptVersion { get; init; } = new("~4.1.0");
     public bool HasPrepatcher { get; init; }
     public List<string>? Incompatibilities { get; init; } = [];
     public Dictionary<string, Range>? ModDependencies { get; init; } = new()
