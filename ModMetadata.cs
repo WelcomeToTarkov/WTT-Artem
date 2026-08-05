62a0dce92fefd8293232176674900ddf49378f57
bushtail <evannosich@icloud.com>
2026-08-05 18:56:12 -0400

    use assembly version

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -12,7 +12,7 @@ public record ModMetadata : IModMetadata
     public string Name { get; init; } = "Artem";
     public string Author { get; init; } = "Crackbone";
     public List<string>? Contributors { get; init; } = ["Crackbone", "The WTT Team"];
-    public Version Version { get; init; } = new("3.0.0");
+    public Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version!.ToString(3));
     public Range SptVersion { get; init; } = new("~4.1.0");
     public bool HasPrepatcher { get; init; }
     public List<string>? Incompatibilities { get; init; } = [];
